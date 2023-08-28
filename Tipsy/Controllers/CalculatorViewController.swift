c0a9551626677452af8813eb76a0d00eb89f0567
AMA2581 <amir.abravesh.ama@gmail.com>
2023-08-28 10:51:38 +0330

    step 6 done

--- Tipsy/Controllers/CalculatorViewController.swift
+++ Tipsy/Controllers/CalculatorViewController.swift
@@ -15,24 +15,28 @@ class CalculatorViewController: UIViewController {
     @IBOutlet var twentyPctButton: UIButton!
     @IBOutlet var splitNumberLabel: UILabel!
 
-    var tipPct = 0.0
+    var tipPct: Float = 0.0
 
     @IBAction func tipChanged(_ sender: UIButton) {
         zeroPctButton.isSelected = false
         tenPctButton.isSelected = false
         twentyPctButton.isSelected = false
+        billTextField.endEditing(true)
 
         sender.isSelected = true
     }
 
     @IBAction func stepperValueChanged(_ sender: UIStepper) {
+        billTextField.endEditing(true)
         splitNumberLabel.text = "\(Int(sender.value))"
     }
 
     @IBAction func calculatePressed(_ sender: UIButton) {
         tipPctCheck()
-        print(tipPct)
-        print(splitNumberLabel.text!)
+//        print(billTextField.text ?? "nothing")
+//        print(tipPct)
+//        print(splitNumberLabel.text!)
+        print(calculate())
     }
 
     func tipPctCheck() {
@@ -45,5 +49,13 @@ class CalculatorViewController: UIViewController {
         if twentyPctButton.isSelected {
             tipPct = 0.2
         }
+    }
+    
+    func calculate() -> Float {
+        var result: Float = 0.0
+        result = Float(billTextField.text!)! * tipPct
+        result += Float(billTextField.text!)!
+        result = result / Float(splitNumberLabel.text!)!
+        return Float(round(100 * result) / 100)
     }
 }
